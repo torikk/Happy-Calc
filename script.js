0e34073d12aa43d0a3975ddbab2b6a3e48e6c3b3
torikk <torikklein@gmail.com>
2018-08-08 17:54:46 -0500

    First round of JS added

--- script.js
+++ script.js
@@ -1,64 +1,87 @@
+/*jshint esversion: 6 */
 
 function dateGreeting() {
-    var hour = new Date().getHours(); 
+    var hour = new Date().getHours();
     var greeting;
-    if (hour < 18) {
-        greeting = "Good day";
+    if (hour < 12) {
+        greeting = "Good morning!";
+    } else if (hour < 18) {
+        greeting = "Good day!";
     } else {
-        greeting = "Good evening";
+        greeting = "Good evening!";
     }
     document.getElementById("AnswerContainer").innerHTML = greeting;
 }
-//CALCULATOR FUNCTIONS
 
-function mathCalc() {
-    
-    const calculator = document.querySelector('.calculator')
-    const keys = calculator.querySelector('.calculatorKeys')
-    
-    document.getElementById("AnswerContainer").innerHTML = result;
-    
+// NOTE: 
+// This is the final source code file for a blog post "How to build a calculator". You can follow the lesson at https://zellwk.com/blog/calculator-part-1
+
+const calculate = (n1, operator, n2) => {
+  let result = ''
+  if (operator === 'add') {
+    result = parseFloat(n1) + parseFloat(n2)
+  } else if (operator === 'subtract') {
+    result = parseFloat(n1) - parseFloat(n2)
+  } else if (operator === 'multiply') {
+    result = parseFloat(n1) * parseFloat(n2)
+  } else if (operator === 'divide') {
+    result = parseFloat(n1) / parseFloat(n2)
+  }
+
+  return result
 }
 
+const calculator = document.querySelector('.calculator')
+const display = calculator.querySelector('#AnswerContainer')
+const keys = calculator.querySelector('.calculator__keys')
 
-    var result = 0;
-    var prevEntry = 0;
-    var operation = null;
-    var currentEntry = '0';
-    
-        
-     $(".calc-btn").click(function(){
-        var buttonPressed = $(this).html();
-         
-        
-        
-          document.getElementById("ProblemContainer").innerHTML = buttonPressed;
-          document.getElementById("AnswerContainer").innerHTML = buttonPressed;
-         
-    });
-    
-    
-    
-});
+keys.addEventListener('click', e => {
+  if (e.target.matches('button')) {
+    const key = e.target
+    const action = key.dataset.action
+    const keyContent = key.textContent
+    const displayedNum = display.textContent
+    const previousKeyType = calculator.dataset.previousKeyType
 
-        if (buttonPressed === AC) {
-            result = 8;
-            currentEntry = 9;
-            return(result);
-        
-         } else if (buttonPressed < 9 && buttonPressed > 0) {
-            currentEntry += buttonPressed;
-            prevEntry += buttonPressed;
-             
-         } else if (buttonPressed > 9 && buttonPressed < 0) {
-            currentEntry += buttonPressed;
-            prevEntry += buttonPressed;
-              
-         } else if (buttonPressed === =) {
-            result = prevEntry;
-          return(result);
-         
-         }
-         
-    
+    Array.from(key.parentNode.children)
+      .forEach(k => k.classList.remove('is-depressed'))
+
+    if (!action) {
+      if (displayedNum === '0' || previousKeyType === 'operator') {
+        display.textContent = keyContent
+      } else {
+        display.textContent = displayedNum + keyContent
+      }
+    }
+
+    if (action === 'decimal') {
+      display.textContent = displayedNum + '.'
+    }
+
+    if (
+      action === 'add' ||
+      action === 'subtract' ||
+      action === 'multiply' ||
+      action === 'divide'
+    ) {
+      key.classList.add('is-depressed')
+      calculator.dataset.previousKeyType = 'operator'
+      calculator.dataset.firstValue = displayedNum
+      calculator.dataset.operator = action
+    }
+
+    if (action === 'clear') {
+      console.log('clear key!')
+    }
+
+    if (action === 'calculate') {
+      const firstValue = calculator.dataset.firstValue
+      const operator = calculator.dataset.operator
+      const secondValue = displayedNum
+
+      display.textContent = calculate(firstValue, operator, secondValue)
+    }
+  }
+})
+
 
